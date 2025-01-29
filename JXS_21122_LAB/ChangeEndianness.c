8e9f68959e39008b8c2426a0f08458cc22ddf90e
V.V.T. <vasilich.tregub@gmail.com>
2025-01-29 12:46:43 +0700

    C to HDL

---
+++ JXS_21122_LAB/ChangeEndianness.c
@@ -0,0 +1,26 @@
+﻿// LZCC.c : Defines the entry point for the application.
+//
+/*LZSS Compression Component*/
+/*Jonathan P Dawson 2014-07.10*/
+
+const int N = 128;
+
+unsigned big_endian = input("big_endian");
+unsigned little_endian = output("little_endian");
+
+void main() {
+
+    unsigned msbyte, lsbyte;
+    unsigned ix;
+
+    unsigned new_size;
+
+    while (1) {
+        for (ix = 0; ix < N / 2; ix++) {
+            msbyte = fgetc(big_endian);
+            lsbyte = fgetc(big_endian);
+            fputc(lsbyte, little_endian);
+            fputc(msbyte, little_endian);
+        }
+    }
+}
